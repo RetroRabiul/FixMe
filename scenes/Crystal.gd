40e636f35318bc1d54470fd507e3f120a1ee879b
RetroRabiul <rabiulcaffe@gmail.com>
2024-05-08 20:48:29 +0600

    sound

--- scenes/Crystal.gd
+++ scenes/Crystal.gd
@@ -6,11 +6,14 @@ func _on_Detect_body_entered(body):
 	if body.is_in_group("player"):
 		collectingSound.play()
 		GlobalSignals.emit_signal("crystal_collected")
+		$CPUParticles2D.emitting = true
 		yield(collectingSound, "finished")
+		$CPUParticles2D.emitting = false
 #		_self_delete()
-#		call_deferred("_self_delete")
-		queue_free()
+		call_deferred("_self_delete")
+		
+#		queue_free()
 
-#
-#func _self_delete():
-#	queue_free()
+
+func _self_delete():
+	queue_free()
