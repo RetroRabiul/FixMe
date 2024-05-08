9c3cf57268f8bd5c27bedcba7de8b523145944da
RetroRabiul <rabiulcaffe@gmail.com>
2024-05-08 22:46:51 +0600

    audio

--- scenes/Crystal.gd
+++ scenes/Crystal.gd
@@ -4,15 +4,18 @@ onready var collectingSound = $CollectedSound
 
 func _on_Detect_body_entered(body):
 	if body.is_in_group("player"):
+		$Detect/CollisionShape2D.set_deferred("disabled", true)
+		$Sprite.hide()
+		
 		collectingSound.play()
 		GlobalSignals.emit_signal("crystal_collected")
 		$CPUParticles2D.emitting = true
 		yield(collectingSound, "finished")
-		$CPUParticles2D.emitting = false
-#		_self_delete()
-		call_deferred("_self_delete")
 		
-#		queue_free()
+		var tween = create_tween()
+		tween.tween_property($CPUParticles2D, "modulate:a", 0.0, 1.0)
+		yield(tween, "finished")
+		call_deferred("_self_delete")
 
 
 func _self_delete():
